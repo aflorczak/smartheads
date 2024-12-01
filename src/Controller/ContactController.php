1fd5d6e1a9690ba9c9239911d03fb1a57f37a158
Adrian Florczak <kontakt@adrianflorczak.pl>
2024-12-01 14:31:38 +0100

    Contact Page: Basic version

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ContactController extends AbstractController
+{
+
+    #[Route('/kontakt', name: 'contact')]
+    public function contact(): Response
+    {
+        return $this->render('contact/index.html.twig');
+    }
+}
